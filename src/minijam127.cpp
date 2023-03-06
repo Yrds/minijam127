afd0301f90ddd9be0b2e59ed38f8d72fe9c8f10a
Yuri Santos <yrds96@protonmail.com>
2023-03-05 21:40:10 -0300

    added animation system

--- src/minijam127.cpp
+++ src/minijam127.cpp
@@ -21,6 +21,7 @@ enum CatState {
 struct Animation {
   Texture2D texture;
   int frames;
+  int speed = 60;
 };
 
 
@@ -35,7 +36,8 @@ struct Cat {
   float attackingTime = 0;
   float attackCoolDown = 30;
   Animation currentAnimation;
-  float currentFrame = 0;
+  int currentFrame = 0;
+  int animationStep = 0;
 };
 
 std::array<Cat, 2> cats;
@@ -43,7 +45,9 @@ std::array<Cat, 2> cats;
 std::vector<Animation> catImages;
 
 void initAssets() {
-  catImages.push_back({LoadTexture("assets/cat_0_idle_1.png"), 6});
+  catImages.push_back({LoadTexture("assets/cat_0_idle_1.png"), 2});
+  catImages.push_back({LoadTexture("assets/cat_0_run.png"), 8, 7});
+  catImages.push_back({LoadTexture("assets/cat_0_attack.png"), 6, 5});
 }
 
 void startGame() {
@@ -52,19 +56,19 @@ void startGame() {
   cats[1].flip = -1;
 }
 
-Rectangle getCatRec(const Cat &cat) {
-  return {
-    cat.position.x,
-    cat.position.y,
-    cat.position.x + (catImages[0].texture.width * cat.scale / 2.0f),
-    cat.position.y + (catImages[0].texture.height * cat.scale / 2.0f)
-  };
-}
+//Rectangle getCatRec(const Cat &cat) {
+//  return {
+//    cat.position.x,
+//    cat.position.y,
+//    cat.position.x + (catImages[0].texture.width * cat.scale / 2.0f),
+//    cat.position.y + (catImages[0].texture.height * cat.scale / 2.0f)
+//  };
+//}
 
 void drawCatPosition(const Cat &cat) {
-  const auto frameWidth = static_cast<float>(cat.currentAnimation.texture.width * cat.scale / cat.currentAnimation.frames);
-  const auto frameHeight = static_cast<float>(catImages[0].texture.height) * cat.scale;
-  DrawRectangleLines(cat.position.x - (frameWidth / 2.0f), cat.position.y - (frameHeight / 2.0f), catImages[0].texture.width * cat.scale, catImages[0].texture.height * cat.scale, RED);
+  const auto frameWidth = static_cast<float>(cat.currentAnimation.texture.width) * cat.scale / cat.currentAnimation.frames;
+  const auto frameHeight = static_cast<float>(cat.currentAnimation.texture.height) * cat.scale;
+  DrawRectangleLines(cat.position.x - (frameWidth / 2.0f), cat.position.y - (frameHeight / 2.0f), frameWidth, frameHeight, RED);
 }
 
 void drawCats() {
@@ -72,8 +76,8 @@ void drawCats() {
     auto frameWidth = static_cast<float>(cat.currentAnimation.texture.width) / cat.currentAnimation.frames;
     auto frameHeight = static_cast<float>(catImages[0].texture.height);
     DrawTexturePro(
-        catImages[0].texture,
-        {0.0f, 0.0f, frameWidth * (cat.flip), frameHeight},
+        cat.currentAnimation.texture,
+        {cat.currentFrame * frameWidth, 0.0f, frameWidth * (cat.flip), frameHeight},
         {cat.position.x - (frameWidth / 2.0f * cat.scale), cat.position.y - (frameHeight / 2.0f * cat.scale), frameWidth * cat.scale, frameHeight * cat.scale},
         {0.0f, 0.0f},
         0.0f,
@@ -85,9 +89,11 @@ void drawCats() {
 
 void processAnimation() {
   for(auto& cat: cats) {
-    cat.currentFrame++;
-    if(cat.currentFrame > cat.currentAnimation.frames) {
-      cat.currentFrame = 0;
+    cat.animationStep = (cat.animationStep + 1) % cat.currentAnimation.speed;
+    std::cout << "next step" << cat.animationStep << std::endl;
+    if(cat.animationStep == 0) {
+      cat.currentFrame = (cat.currentFrame + 1) % 100;
+      std::cout << "next frame" << std::endl;
     }
   }
 }
@@ -141,12 +147,17 @@ void processCatsState() {
       case IDLE:
         cat.direction = 0;
         cat.attackingTime = 0;
+        cat.currentAnimation = catImages[0];
+        break;
       case WALKING_DOWN:
       case WALKING_UP:
+        cat.currentAnimation = catImages[1];
         cat.position.y += cat.direction;
+        break;
       case ATTACKING:
+        cat.currentAnimation = catImages[2];
         cat.attackingTime++;
-      break;
+        break;
     }
   }
 }
@@ -156,6 +167,7 @@ void frame() {
   processPlayerControl();
   changeCatsState();
   processCatsState();
+  processAnimation();
   
   // DRAW
   BeginDrawing();
