f8d906717dc180df0bf64757a4f8650876b64973
Yuri dos Santos <yrds96@protonmail.com>
2023-03-06 05:01:07 -0300

    web setup configuration

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "${MESON_SOURCE_ROOT}"
-cd ${MESON_BUILD_ROOT} && zip -r ${MESON_SOURCE_ROOT}/2048im.zip ./index*
+cd ${MESON_BUILD_ROOT} && zip -r ${MESON_SOURCE_ROOT}/game.zip ./index*
